727a4dc8e940caa0ffdfc38bebcc4cb56d7b9892
AlanDecode <zytbuaa1415@gmail.com>
2020-01-05 17:41:28 +0800

    Update my site

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ enable_jsdelivr = {
     "repo": "AlanDecode/site-Blog@gh-pages"
 }
 category_by_folder = True
-for_manual_build_trigger = 0
+for_manual_build_trigger = 1
 
 # 站点设置
 site_name = "无文字 | 三无计划"
